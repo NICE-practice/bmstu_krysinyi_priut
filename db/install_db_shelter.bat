d5f70e3964a026c7b0be438d63d470a661559fe0
kpirap18 <ir1nak0z@yandex.ru>
2023-03-31 01:52:52 +0300

    #47 Edit workflow

---
+++ db/install_db_shelter.bat
@@ -0,0 +1,9 @@
+psql -h 127.0.0.1 "sslmode=disable user=postgres port=5432 password=%1%" < ./create_db_shelter.sql
+
+pip install psycopg2
+pip install faker_vehicle
+pip install faker
+pip install random
+pip install animals.py
+
+python generate_db_shelter.py
--- db/install_db_shelter.sh
+++ db/install_db_shelter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-psql -h 127.0.0.1 "sslmode=disable user=postgres port=5432 password=1234" < ./create_db_shelter.sql
+psql -h 127.0.0.1 "sslmode=disable user=postgres port=5432 password=$1" < ./create_db_shelter.sql
 
 pip3 install psycopg2
 pip3 install faker_vehicle
