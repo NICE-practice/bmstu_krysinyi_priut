ba9f60f47ce44db06d15a38900837305e4fc13a6
Alena Zaytseva <alena_zaytseva_02@mail.ru>
2023-04-03 16:58:26 +0300

    #18 add: router and controller for jwt-authorization

---
+++ src/server/controllers/userController.js
@@ -0,0 +1,62 @@
+const ApiError = require("../errors/ApiError")
+const bcrypt = require('bcrypt')
+const {UserShelter, DictPrivelege} = require('../models/modelsORM')
+const jwt = require('jsonwebtoken')
+
+const generateJwt = (userId, userLogin, dictPrivilegePrivId) => {
+    return jwt.sign(
+        {userId, userLogin, dictPrivilegePrivId},
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
+
+class UserController {
+    // tmp
+    async addUser(req, res, next) {
+        const {userFIO, userLogin, userCheck, dictPrivilegePrivId} = req.body
+        
+        if (!userLogin || !userCheck) {
+            return next(ApiError.badRequest('Incorrect userLogin or userCheck'))
+        }
+        const candidate = await UserShelter.findOne({where: {userLogin}})
+        if (candidate) {
+            return next(ApiError.badRequest('User with such userLogin already exists'))
+        }
+
+        const privilege = await DictPrivelege.findByPk(dictPrivilegePrivId)
+        if (!privilege) {
+            return next(ApiError.badRequest('Such privilege does not exist'))
+        }
+
+        const hashPassword = await bcrypt.hash(userCheck, 5)
+        const user = await UserShelter.create({userFIO, userLogin, userCheck: hashPassword, dictPrivilegePrivId})
+        console.log(user)
+        console.log(userFIO, userLogin, userCheck, dictPrivilegePrivId)
+        const token = generateJwt(user.userId, user.userLogin, user.dictPrivilegePrivId)
+        return res.json({token})
+    }
+
+    async login(req, res, next) {
+        const {userLogin, userCheck} = req.body
+        const user = await UserShelter.findOne({where: {userLogin}})
+        if (!user) {
+            return next(ApiError.internal('User with such userLogin does not exist'))
+        }
+        let comparePassword = bcrypt.compareSync(userCheck, user.userCheck)
+        if (!comparePassword) {
+            return next(ApiError.internal('Incorrect userCheck'))
+        }
+        const token = generateJwt(user.userId, user.userLogin, user.dictPrivilegePrivId)
+        return res.json({token})
+    }
+
+    // пересоздание токена
+    async check(req, res) {
+        console.log(req.user)
+        const token = generateJwt(req.user.userId, req.user.userLogin, req.user.dictPrivilegePrivId)
+        return res.json({token})
+    }
+}
+
+module.exports = new UserController()
