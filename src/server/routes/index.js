a5f2b1736318f6f96259ca7ee0d3edd78d22bec9
Alena Zaytseva <alena_zaytseva_02@mail.ru>
2023-04-04 10:37:01 +0300

    #19 add: controller and router for animal

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,7 +2,9 @@ const Router = require("express");
 
 const router = new Router();
 const userRouter = require("./userRouter");
+const animalRouter = require("./animalRouter");
 
 router.use("/user", userRouter);
+router.use("/animal", animalRouter);
 
 module.exports = router;
