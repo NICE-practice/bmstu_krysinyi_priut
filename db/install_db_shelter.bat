eea3d68603d841e6132dd7622d2668b3f3488da7
kpirap18 <ir1nak0z@yandex.ru>
2023-04-12 00:58:26 +0300

    #67 Fix bat install file for windows

--- db/install_db_shelter.bat
+++ db/install_db_shelter.bat
@@ -1,7 +1,3 @@
 psql -h 127.0.0.1 "sslmode=disable user=postgres port=5432 password=%1" < ./create_db_shelter.sql
 
-pip install psycopg2
-pip install faker
-pip install animals.py
-
-python generate_db_shelter.py %1
+psql -h 127.0.0.1 "sslmode=disable user=postgres port=5432 password=%1" < ./filling_db_shelter.sql 
