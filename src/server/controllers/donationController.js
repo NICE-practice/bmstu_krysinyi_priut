e5291abd91c557ad26d8a88b2d1f2d66c7ecdeff
Alena Zaytseva <alena_zaytseva_02@mail.ru>
2023-04-13 18:20:03 +0300

    #61 add tests for donation API, add error throwing in controller

--- src/server/controllers/donationController.js
+++ src/server/controllers/donationController.js
@@ -1,19 +1,24 @@
 const { Donation } = require("../models/modelsORM");
 const DonationDTO = require("../models/donationDTO");
+const ApiError = require("../errors/ApiError");
 
 class DonationController {
-  async add(req, res) {
+  async add(req, res, next) {
     const { donationName, donationSum } = req.body;
     const { donationId } = req.body;
 
-    const donationORM = await Donation.create({
-      donationId,
-      donationName,
-      donationSum,
-    });
+    try {
+      const donationORM = await Donation.create({
+        donationId,
+        donationName,
+        donationSum,
+      });
 
-    const donationDTO = new DonationDTO(donationORM);
-    return res.json(donationDTO);
+      const donationDTO = new DonationDTO(donationORM);
+      return res.json(donationDTO);
+    } catch (err) {
+      return next(ApiError.internal(err.message));
+    }
   }
 }
 
