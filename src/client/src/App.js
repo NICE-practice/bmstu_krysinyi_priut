c7a442b547266b0d43be70dc53752dc33f7daa8a
twhvee <vi.sysoeva1506@gmail.com>
2023-04-16 11:10:53 +0300

    #35 add authorization page and css file

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,17 +1,37 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./conponents/AppRouter";
 import { observer } from "mobx-react-lite";
-
 import Footer from "./conponents/Footer";
 import { Context } from ".";
 import { fetchInfo } from "./http/shelterInfoApi";
+import { check } from "./http/userApi";
+import { Spinner } from "react-bootstrap";
 
 const App = observer(() => {
+  const [loading, setLoading] = useState(true);
   const { info } = useContext(Context);
+  const { user } = useContext(Context);
   useEffect(() => {
     fetchInfo().then((data) => info.setInfoShelter(data));
-  });
+    check()
+      .then((data) => {
+        user.setUser(data);
+        user.setIsAuth(true);
+        user.setPrivilege(data.dictPrivilegePrivId);
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <Spinner animation={"grow"} />;
+  }
+
+  console.log("app");
+  console.log(user.privilege);
+  console.log(user.isAuth);
+  // const [loading, setLoading] = useState(true);
+
   return (
     <BrowserRouter>
       <AppRouter />
