e7619386b420e2e120905192772a94cc96d31049
twhvee <vi.sysoeva1506@gmail.com>
2023-04-19 14:59:19 +0300

    #33 add form for messages and css file

--- src/client/src/pages/Contact.js
+++ src/client/src/pages/Contact.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import FormContact from "../conponents/FormContact";
 
 export default function Contact() {
   return (
     <div>
       Contact
+      <FormContact />
     </div>
-  )
+  );
 }
