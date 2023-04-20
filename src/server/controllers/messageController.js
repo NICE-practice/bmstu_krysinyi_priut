cc7bc3cda47a8dbbb87f1894bf02ca2612443963
Alena Zaytseva <alena_zaytseva_02@mail.ru>
2023-04-20 12:17:38 +0300

    #65 add documentation for messages API

--- src/server/controllers/messageController.js
+++ src/server/controllers/messageController.js
@@ -2,8 +2,156 @@ const { Message } = require("../models/modelsORM");
 const ApiError = require("../errors/ApiError");
 const MessageDTO = require("../models/messageDTO");
 
+/**
+ * @swagger
+ * tags:
+ *   name: Messages
+ *   description: The messages managing API
+ * /message:
+ *   post:
+ *     summary: Send a new message
+ *     tags: [Messages]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Message'
+ *     responses:
+ *       200:
+ *         description: The sent message.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Message'
+ *       500:
+ *         description: Internal server error
+ * */
+/**
+ * @swagger
+ * /message:
+ *   get:
+ *     security:
+ *        - bearerAuth: []
+ *     summary: Get messages list
+ *     tags: [Messages]
+ *     parameters:
+ *       - in: query
+ *         name: offset
+ *         required: false
+ *         schema:
+ *           type: integer
+ *         allowReserved: true
+ *         description: The number of items to skip before starting to collect the result set
+ *       - in: query
+ *         name: limit
+ *         required: false
+ *         schema:
+ *           type: integer
+ *         allowReserved: true
+ *         description: The numbers of items to return
+ *     responses:
+ *       200:
+ *         description: The list of messages
+ *         content:
+ *           application/json:
+ *             schema:
+ *              type: object
+ *              properties:
+ *                messagesCount:
+ *                  type: integer
+ *                  description: The total amount of messages in DB
+ *                messages:
+ *                  type: array
+ *                  items:
+ *                    $ref: '#/components/schemas/Message'
+ *                  description: Messages list
+ *       401:
+ *         description: Header didn't contain authorization token
+ *       403:
+ *         description: Role of the user doesn't give access for this call
+ *       500:
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /message/{messageId}:
+ *   get:
+ *     security:
+ *        - bearerAuth: []
+ *     summary: Get a message by id
+ *     tags: [Messages]
+ *     parameters:
+ *       - in: path
+ *         name: messageId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         allowReserved: true
+ *         description: The id of message to find
+ *     responses:
+ *       200:
+ *         description: Message with the specified Id
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Message'
+ *       401:
+ *         description: Header didn't contain authorization token
+ *       403:
+ *         description: Role of the user doesn't give access for this call
+ *       404:
+ *         description: Message with specified id doesn't exist
+ *       500:
+ *         description: Internal server error
+ */
+
+/**
+ * @swagger
+ * /message/{messageId}:
+ *   patch:
+ *     security:
+ *        - bearerAuth: []
+ *     summary: Change answered flag of a message by it's id (mark the message answered or not answered)
+ *     tags: [Messages]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *             schema:
+ *                type: object
+ *                properties:
+ *                  answerFlag:
+ *                    type: boolean
+ *                    description: The value of answerFlag to set
+ *     parameters:
+ *       - in: path
+ *         name: messageId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         allowReserved: true
+ *         description: The id of message to update
+ *     responses:
+ *       200:
+ *         description: Updated message
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Message'
+ *       401:
+ *         description: Header didn't contain authorization token
+ *       403:
+ *         description: Role of the user doesn't give access for this call
+ *       404:
+ *         description: Message with specified id doesn't exist
+ *       500:
+ *         description: Internal server error
+ */
+
 class MessageController {
-  async add(req, res) {
+  async add(req, res, next) {
     const {
       messageName,
       phone,
@@ -11,21 +159,25 @@ class MessageController {
       messageText,
       preferredContactMethod,
       answerFlag,
-    } = req.body;
-    const { messageId } = req.body;
-
-    const messageORM = await Message.create({
       messageId,
-      messageName,
-      phone,
-      email,
-      messageText,
-      preferredContactMethod,
-      answerFlag,
-    });
+    } = req.body;
 
-    const messageDTO = new MessageDTO(messageORM);
-    return res.json(messageDTO);
+    try {
+      const messageORM = await Message.create({
+        messageId,
+        messageName,
+        phone,
+        email,
+        messageText,
+        preferredContactMethod,
+        answerFlag,
+      });
+
+      const messageDTO = new MessageDTO(messageORM);
+      return res.json(messageDTO);
+    } catch (err) {
+      return next(ApiError.internal(err.message));
+    }
   }
 
   async getList(req, res) {
--- src/server/index.js
+++ src/server/index.js
@@ -6,18 +6,77 @@ const sequelize = require("./db");
 const router = require("./routes/index");
 const errorHandler = require("./middleware/ErrorHandlingMiddleware");
 
+///////////////////////////////////
+const bodyParser = require("body-parser");
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+/////////////////////////////
+
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", router);
-app.use(errorHandler); // обязательно в конце
+//app.use(errorHandler); // обязательно в конце
 
 const start = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
+
+    ///////////////////////////////////
+    app.use(
+      bodyParser.urlencoded({
+        extended: true,
+      })
+    );
+    app.use(bodyParser.json());
+
+    const options = {
+      definition: {
+        openapi: "3.0.0",
+        info: {
+          title: "Petshelter backend API documentation",
+          version: "0.1.0",
+          description: "Simple CRUD API operations",
+        },
+        components: {
+          securitySchemes: {
+            bearerAuth: {
+              type: "http",
+              scheme: "bearer",
+              bearerFormat: "JWT",
+            },
+          },
+          // securityDefinitions: {
+          //   Bearer: {
+          //     type: "apiKey",
+          //     name: "Authorization",
+          //     in: "header",
+          //     description:
+          //       'Enter the token with the `Bearer: ` prefix, e.g. "Bearer abcde12345".',
+          //   },
+        },
+        servers: [{ url: "http://localhost:5000/api" }],
+      },
+      // apis: ["./routes/*.js"],
+      apis: [
+        "./routes/index.js",
+        "./routes/*.js",
+        "./models/*.js",
+        "./controllers/*.js",
+      ],
+    };
+
+    const specs = swaggerJsdoc(options);
+    app.use(
+      "/api-docs",
+      swaggerUi.serve,
+      swaggerUi.setup(specs, { explorer: true })
+    );
+    ////////////////////////////////////////////
+
     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   } catch (e) {
     console.log(e);
