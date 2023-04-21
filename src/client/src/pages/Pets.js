59978140b84efe9a673983803d9119eb71fe28ab
twhvee <vi.sysoeva1506@gmail.com>
2023-04-21 22:42:44 +0300

    #38 add filter for pets and css styles

--- src/client/src/pages/Pets.js
+++ src/client/src/pages/Pets.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import Menu from "../conponents/Menu";
+import { observer } from "mobx-react-lite";
+import { Context } from "../index";
+import { fetchPets } from "../http/petsApi";
 
-export default function Cats() {
+const Cats = observer(() => {
+  const { pet } = useContext(Context);
+
+  useEffect(() => {
+    if (pet.age !== 0) {
+      fetchPets(pet.breed, pet.type, pet.age, pet.page, 15).then((data) => {
+        console.log(data);
+        pet.setPet(data.animals);
+        pet.setTotalCount(data.animalsCount);
+      });
+    } else {
+      fetchPets(pet.breed, pet.type, null, pet.page, 15).then((data) => {
+        console.log(data);
+        pet.setPet(data.animals);
+        pet.setTotalCount(data.animalsCount);
+      });
+    }
+  }, [pet.breed, pet.type, pet.age, pet.page]);
+
   return (
     <div>
       <Menu />
-      Cats
+      Pets
     </div>
   );
-}
+});
+export default Cats;
