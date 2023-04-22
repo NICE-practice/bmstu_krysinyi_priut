28e68914edd94286a3db50f8374ee924dd4c78f4
twhvee <vi.sysoeva1506@gmail.com>
2023-04-23 00:01:36 +0300

    #28 add slider pets and styles css

--- src/client/src/conponents/PetCard.js
+++ src/client/src/conponents/PetCard.js
@@ -4,8 +4,8 @@ import "../style/Home.css";
 export default function PetCard(pet) {
   return (
     <div className="cat_inf">
-      <img className="cat_img" src={pet.animalimg} />
-      <h2 className="cat_title2">{pet.animalname}</h2>
+      <img className="cat_img" src={pet.pet.animalImg} />
+      <h2 className="cat_title2">{pet.pet.animalName}</h2>
       <button className="cat_but">Узнать больше</button>
     </div>
   );
---
+++ src/client/src/conponents/SliderPet.js
@@ -0,0 +1,73 @@
+import { toJS } from "mobx";
+import React, { useContext, useRef, useState } from "react";
+import { observer } from "mobx-react-lite";
+import { Context } from "../index";
+import PetCard from "./PetCard";
+import "../style/Home.css";
+
+const SliderPet = observer(() => {
+  const { pet } = useContext(Context);
+  const [prev, setPrev] = useState(false);
+  const [next, setNext] = useState(false);
+  const slider = useRef(null);
+  let pets = toJS(pet.pets);
+  console.log(pets);
+  let position = 0;
+  const prevHandler = () => {
+    console.log("prev");
+    console.log(position);
+    if (position === 2 * 400 + 400) {
+      setPrev(true);
+      setNext(false);
+    } else {
+      setPrev(false);
+      position += 400;
+      slider.current.childNodes.forEach((element) => {
+        element.style = `transform: translateX(${position}px)`;
+      });
+    }
+  };
+  const nextHandler = () => {
+    console.log("next");
+    console.log(position);
+    if (position === -4 * 400 + 400) {
+      setNext(true);
+      setPrev(false);
+    } else {
+      setNext(false);
+      position += -400;
+      slider.current.childNodes.forEach((element) => {
+        if (element !== pets[7]) {
+          element.style = `transform: translateX(${position}px)`;
+        }
+      });
+    }
+  };
+  return (
+    <div id="cats" className="cats">
+      <h1 className="cat_title">Наши пушистики, которые ищут дом</h1>
+      <div className="slider_content">
+        <div className="arrow_cats" disabled={prev} onClick={prevHandler}>
+          <p>&larr;</p>
+        </div>
+        <div className="cat_block" ref={slider}>
+          <PetCard pet={pets[0]} />
+          <PetCard pet={pets[1]} />
+          <PetCard pet={pets[2]} />
+          <PetCard pet={pets[3]} />
+          <PetCard pet={pets[4]} />
+          <PetCard pet={pets[5]} />
+          <PetCard pet={pets[6]} />
+          <PetCard pet={pets[7]} />
+          <PetCard pet={pets[8]} />
+        </div>
+        <div className="arrow_cats" disabled={next} onClick={nextHandler}>
+          <p>&rarr;</p>
+        </div>
+      </div>
+
+      <button className="cats_but">Наши животные</button>
+    </div>
+  );
+});
+export default SliderPet;
